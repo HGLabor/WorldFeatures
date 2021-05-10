d72236a2b751d2a49562156bc9ce20ca1887c225
mooziii <mooz@moozunity.de>
2021-05-10 12:45:56 +0200

    Create ContributorFeature.java

---
+++ src/main/java/de/hglabor/worldfeatures/features/util/ContributorFeature.java
@@ -0,0 +1,24 @@
+package de.hglabor.worldfeatures.features.util;
+
+import de.hglabor.worldfeatures.features.Feature;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class ContributorFeature extends Feature {
+
+    public ContributorFeature() {
+        super("ContributorStar");
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if(!isEnabled) {
+            return;
+        }
+        Player player = event.getPlayer();
+        if(player.hasPermission("hglabor.survival.contributor")) {
+            player.setPlayerListName("§e⭐ §7" + player.getPlayerListName());
+        }
+    }
+}
