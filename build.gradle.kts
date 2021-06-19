52edb64f6c017ce75e17ed0a3a7ba14a3c90e99e
mooziii <mooz@moozunity.de>
2021-06-19 20:55:33 +0200

    Use spigot "libraries" feature instead of shading it

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ plugins {
 group = "de.hglabor"
 version = "1.0-SNAPSHOT"
 
-java.sourceCompatibility = JavaVersion.VERSION_1_8
-java.targetCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_16
+java.targetCompatibility = JavaVersion.VERSION_16
 
 repositories {
     jcenter()
@@ -37,13 +37,12 @@ tasks {
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
+    compileOnly(kotlin("stdlib"))
     compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
     compileOnly("net.luckperms:api:5.2")
     compileOnly("org.bukkit","craftbukkit","1.16.5-R0.1-SNAPSHOT")
-    implementation("net.axay:kspigot:1.16.28")
-    implementation("de.hglabor:hglabor-utils:0.0.11")
     compileOnly("net.axay:kspigot:1.17.1")
+    compileOnly("de.hglabor:hglabor-utils:0.0.11")
     implementation("net.axay:BlueUtils:1.0.2")
     implementation("org.litote.kmongo:kmongo-core:4.2.3")
     implementation("org.litote.kmongo:kmongo-serialization-mapping:4.2.3")
